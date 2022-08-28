ae9de19c8890ad855f71d41ed7dc7e112583b36b
antishoker <zelenskiy.anton@gmail.com>
2022-08-28 22:09:07 +0300

    1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 
 
 def print_hi(name):
-    """Вычисляет !"""
+    """Вычисляет ! - добавляю"""
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
     c = 1
